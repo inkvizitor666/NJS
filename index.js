ba151ce31a487f9a56ff80e307e3f3fc7f5c273f
Alexey Beloshapkin <Leks11172@mail.ru>
2025-09-27 00:12:44 +0700

    мессаже

--- index.js
+++ index.js
@@ -1,49 +1,40 @@
-const incrementation = (n1,n2) => n1 + n2;
-const decrementation = (n1,n2) => n1 - n2;
-const multiplication = (n1,n2) => n1 * n2;
-const division = (n1,n2) => n1 / n2;
-
-function getOperationFunc(sign)
-{
-        switch(sign)
-        {
-        case '+':
-            return incrementation;
-        case '-':
-            return decrementation;
-        case '*':
-            return multiplication;
-        case '/':
-            return division;
-        default:
-             console.log('ERR');
-             return null;
-        }
+const incrementation = (n1, n2) => n1 + n2;
+const decrementation = (n1, n2) => n1 - n2;
+const multiplication = (n1, n2) => n1 * n2;
+const division = (n1, n2) => n1 / n2;
+// Толяяян!!
+function getOperationFunc(sign) {
+  switch (sign) {
+    case "+":
+      return incrementation;
+    case "-":
+      return decrementation;
+    case "*":
+      return multiplication;
+    case "/":
+      return division;
+    default:
+      console.log("ERR");
+      return null;
+  }
 }
 
-function calculate (...args)
-{
+function calculate(...args) {
+  console.log(args);
 
-    console.log(args);
+  let result = args[0];
 
-    let result = args[0];
-
-    for(let i = 1; i<args.length;i=i+2)
-    {
-        const sign = args[i] 
-        const buf = args[i+1]
-        const currentOperation =  getOperationFunc(sign);
-        if(currentOperation)
-        {
-            result = currentOperation(result,buf);
-        }
-        console.log(i);
+  for (let i = 1; i < args.length; i = i + 2) {
+    const sign = args[i];
+    const buf = args[i + 1];
+    const currentOperation = getOperationFunc(sign);
+    if (currentOperation) {
+      result = currentOperation(result, buf);
     }
+    console.log(i);
+  }
 
-
-    return result;
+  return result;
 }
 
-console.log(calculate(4,'-',6,'*',4,'-',1,'+',7));
-
-
+console.log(calculate(4, "-", 6, "*", 4, "-", 1, "+", 7));
