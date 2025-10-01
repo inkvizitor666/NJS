26d3af9581bf8c9733d35da1d1a8e0b624e6cccb
Анатолий Григорьев <grigorevanatoliy93@gmail.com>
2025-10-01 20:41:55 +0700

    ref(calculate) заменил функцию ParserString на функцию из модуля ParserString

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 //console.log(age1);
 
 const {getOperationFunc} = require('./mathOperations.js');
-
+const {parserString} = require('./parserString.js');
 
 function calculate(str) {
   const args = parserString(str);
@@ -42,51 +42,6 @@ function calculate(str) {
   return result;
 }
 
-function parserString(string) {
-  let arrPars = [];
 
-  for (let i = 0; i < string.length; i++) {
-    const num = Number(string[i]);
-    if (Number.isNaN(num)) {
-      arrPars.push(string[i]);
-    } else {
-      const lastIndex = arrPars.length - 1;
-      const lastElementItem = arrPars[lastIndex];
-      if (!Number.isNaN(Number(lastElementItem))) {
-        arrPars[lastIndex] = Number(`${lastElementItem}${string[i]}`);
-      } else {
-        arrPars.push(num);
-      }
-    }
-  }
-  return arrPars;
-}
-
-function parserStringTest() {
-  const testCases = [
-    {
-      arg: "4-6*4-1+2*6",
-      result: [4, "-", 6, "*", 4, "-", 1, "+", 2, "*", 6],
-    },
-    {
-      arg: "4-6*4-1167+2*336",
-      result: [4, "-", 6, "*", 4, "-", 1167, "+", 2, "*", 336],
-    },
-  ];
-
-  testCases.forEach((testCase) => {
-    if (
-      JSON.stringify(testCase.result) ===
-      JSON.stringify(parserString(testCase.arg))
-    ) {
-      console.log("заебумба");
-    } else {
-      console.log("хуй");
-    }
-    console.log(testCase.result);
-    console.log(parserString(testCase.arg));
-  });
-  return;
-}
 
 console.log(calculate("4-6*4-1167+2*336"));
