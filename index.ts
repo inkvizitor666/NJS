a5a3fa5bfc30e46551bbec7dedcd594e4f901db3
Анатолий Григорьев <grigorevanatoliy93@gmail.com>
2025-10-16 22:20:20 +0700

    feat(index.ts)добавлена функция удаления пользователя из  массива добавлен вывод массива с пользователями

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ app.get("/calculate", (req, res) => {
   name: 'anatoliy',
   age: 12
 }  */
-const dbUsers = [
+let dbUsers = [
   {
     id: "1",
     name: "artem",
@@ -49,65 +49,66 @@ app.get("/user", (req, res) => {
 });
 
 app.post("/user", (req, res) => {
+  const id = Math.ceil(Math.random() * 1000);
+  const name = req.query.name;
+  const age = Number(req.query.age);
 
+  if (typeof name == "string" && !isNaN(age)) {
+    dbUsers.push({
+      id: String(id),
+      name: name,
+      age: age,
+    });
 
-const id = Math.ceil(Math.random() * 1000);
-const name = req.query.name;
-const age = Number(req.query.age);
-
-if(typeof name == 'string' && !isNaN(age))
-{
-
-  dbUsers.push({
-    id: String(id),
-    name: name,
-    age: age
-  })
-
-  res.send(`Пользователь успешно создан ID ${id}`);
-}
-else
-{
-  res.send(`ERR`);
-}
-
+    res.send(`Пользователь успешно создан ID ${id}`);
+  } else {
+    res.send(`ERR`);
+  }
 });
 
-app.put("/user",(req, res) => {
+app.put("/user", (req, res) => {
+  const id = req.query.id;
+  const name = req.query.name;
+  const age = Number(req.query.age);
 
-const id = req.query.id;
-const name = req.query.name;
-const age = Number(req.query.age);
+  if (typeof id == "string" && typeof name == "string" && !isNaN(age)) {
+    const findUserIndex = dbUsers.findIndex((user) => {
+      return user.id == id;
+    });
 
-if(typeof id == "string" && typeof name =="string" && !isNaN(age) )
-{
-   const findUserIndex = dbUsers.findIndex((user) => {
-    return user.id == id;
+    if (findUserIndex != -1) {
+      dbUsers[findUserIndex] = {
+        id: id,
+        name: name,
+        age: age,
+      };
 
+      res.send(`Пользователь с ID:${id} успешно изменён`);
+      return;
+    }
+    res.send(`Пользователь с ID:${id} не найден`);
+  }
+});
 
+app.delete("/user", (req, res) => {
+  const idUser = req.query.id;
 
+  const findUser = dbUsers.find((user) => {
+    return user.id == idUser;
   });
 
-  if (findUserIndex != -1) {
-
-    dbUsers[findUserIndex] = {
-      id: id,
-      name: name,
-      age: age
-    }
-
-    res.send(`Пользователь с ID:${id} успешно изменён`);
+  if (findUser) {
+    const bufDbUser = dbUsers.filter((bufId) => bufId.id !== idUser);
+    dbUsers = bufDbUser;
+    res.send(`Пользователь с ID ${idUser} удален`);
     return;
   }
-  else{
-    res.send(`Пользователь с ID:${id} не найден`);
+  res.send(`Пользователь с ID ${idUser} не найден`);
+});
 
-  }
-
-}
-
-})
-
+app.get("/users", (req, res) => {
+  res.json(dbUsers);
+  return;
+});
 app.listen(3000);
-
 console.log();
