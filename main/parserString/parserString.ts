d187f0a752d075229a3608f3a8af1df406752274
Анатолий Григорьев <grigorevanatoliy93@gmail.com>
2025-10-20 19:31:32 +0700

    ref(типизация) был протипизирован элемент добавляемый в пропарсенный массив

--- main/parserString/parserString.ts
+++ main/parserString/parserString.ts
@@ -1,11 +1,11 @@
-export function parserString(string : string) {
-  let arrPars:(string|number)[] = [];
+export function parserString(string: string) {
+  let arrPars: (string | number)[] = [];
   for (let i = 0; i < string.length; i++) {
     const num = Number(string[i]);
     if (isNaN(num)) {
-      arrPars.push(string[i]);
+      arrPars.push(Number(string[i]));
     } else {
-      const lastIndex:number = arrPars.length - 1;
+      const lastIndex: number = arrPars.length - 1;
       const lastElementItem = arrPars[lastIndex];
       if (!isNaN(Number(lastElementItem))) {
         arrPars[lastIndex] = Number(`${lastElementItem}${string[i]}`);
