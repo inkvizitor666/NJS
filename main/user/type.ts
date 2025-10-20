043a201b648dae57b9681ada830b1ba3b0bccd39
Анатолий Григорьев <grigorevanatoliy93@gmail.com>
2025-10-20 15:36:03 +0700

    ref(TYPE)добавлен файл type.ts с описанием типов (интерфейсы)

---
+++ main/user/type.ts
@@ -0,0 +1,12 @@
+export interface Ifriend {
+  id: string;
+  name: string;
+  age: number;
+  friends: Ifriend[];
+}
+export interface IUser {
+  id: string;
+  name: string;
+  age: number;
+  friends: Ifriend[];
+}
